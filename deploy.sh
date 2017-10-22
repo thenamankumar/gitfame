6388667a755d8718f40ec1449bb87aff7d1c3aa7
Naman Kumar <naman@outlook.in>
2017-10-22 21:27:45 +0530

    Added repo status

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# GH_USER: Username for the user
+# GH_TOKEN: Token or pass for the user
+# GH_REF: github.com/<user-name>/<repo-name>.git
+rm -rf dist/
+polymer build
+cd build/
+git init .
+git config --local user.name "hereisnaman"
+git config --local user.email "naman@outlook.in"
+now=$(date)
+echo "Deployed on $now" >> "Deployed_$now".txt
+git add -A
+git commit -m "Deploy to Build"
+git push --force --quiet "https://${GH_USER}:${GH_TOKEN}@${GH_REF}" build
