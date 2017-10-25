0aa7a3da44fec05446a95739c58d683728f3f268
Naman Kumar <naman@outlook.in>
2017-10-25 10:05:37 +0530

    Added icons in prefetch cache

--- precache-config.js
+++ precache-config.js
@@ -2,12 +2,35 @@ module.exports = {
     staticFileGlobs: [
         'index.html',
         'manifest.json',
-        'icons/icon-16x16.png',
-        'bower_components/webcomponentsjs/webcomponents-loader.js'
+        'icons/favicon-32x32.png',
+        'icons/apple-touch-icon.png',
+        'icons/android-chrome-192x192.png',
+        'icons/favicon-16x16.png',
+        'icons/safari-pinned-tab.svg',
+        'icons/mstile-144x144.png',
+        'bower_components/webcomponentsjs/webcomponents-loader.js',
+        'browserconfig.xml',
+        'icons/android-chrome-36x36.png',
+        'icons/android-chrome-48x48.png',
+        'icons/android-chrome-72x72.png',
+        'icons/android-chrome-96x96.png',
+        'icons/android-chrome-144x144.png',
+        'icons/android-chrome-192x192.png',
+        'icons/android-chrome-256x256.png',
+        'icons/android-chrome-384x384.png'
     ],
     navigateFallback: '/index.hml',
     navigateFallbackWhitelist: [/^\/user\//],
     runtimeCaching: [
+        {
+            urlPattern: '/',
+            handler: 'networkFirst',
+            options: {
+                cache: {
+                    name: 'src'
+                }
+            }
+        },
         {
             urlPattern: /src/,
             handler: 'networkFirst',
