2a3e1411ef383369523946522675787d0033f5e8
hereisnaman <naman@outlook.in>
2017-12-23 02:55:02 +0530

    Deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf build/
 npm i
-echo -e "NODE_ENV=production\nBACKEND_URL=https://backend.gitfa.me\nGIT_ID=$GITHUB_CLIENT_ID\nGIT_SECRET=$GITHUB_CLIENT_SECRET\nGIT_TOKEN=$GITHUB_PERSONAL_TOKEN" >> env/prod.env
+echo -e "NODE_ENV=production\nAPI_URL=https://backend.gitfa.me\nGIT_ID=$GITHUB_CLIENT_ID\nGIT_SECRET=$GITHUB_CLIENT_SECRET\nGIT_TOKEN=$GITHUB_PERSONAL_TOKEN" >> env/prod.env
 npm run build
 chmod 600 key.pem
 ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf /var/www/html/gitfame-beta/*;"
