742d8a03b5b0dc48515d7355834edc63007b7566
Samkit Jain <samkitjain5795@gmail.com>
2017-12-30 19:29:48 +0530

    Using promise.all

--- src/app/components/Comparison.jsx
+++ src/app/components/Comparison.jsx
@@ -9,35 +9,30 @@ class Comparison extends React.Component {
   }
 
   process() {
-    FetchData(this.props.match.params.username1, true)
-      .then((response) => {
-        const data = JSON.parse(response);
-        if (data.success) {
-          return data;
-        }
-        throw new Error('User not found');
-      })
-      .then(data => GenerateStats(data))
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    FetchData(this.props.match.params.username2, true)
-      .then((response) => {
-        const data = JSON.parse(response);
-        if (data.success) {
-          return data;
-        }
-        throw new Error('User not found');
-      })
-      .then(data => GenerateStats(data))
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
+    const fetchPromise = (username) => {
+      FetchData(username, true)
+        .then((response) => {
+          const data = JSON.parse(response);
+          if (data.success) {
+            return data;
+          }
+          throw new Error('User not found');
+        })
+        .then(data => GenerateStats(data))
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    Promise.all([
+      fetchPromise(this.props.match.params.username1),
+      fetchPromise(this.props.match.params.username2),
+    ])
+      .then((responses) => {
+        console.log(responses[0]);
+        console.log(responses[1]);
       });
   }
 
