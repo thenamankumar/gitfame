d731454f124ae2706e06b27fd1a789e24ca1a0ed
Naman Kumar <naman@outlook.in>
2018-04-15 09:28:25 +0530

    Added followers

--- app/components/Analytics.jsx
+++ app/components/Analytics.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
-import Animate from '../components/Animate';
-import RecCurveThree from '../assets/svg/RecCurveThree.svg';
+import { TiGroup } from 'react-icons/lib/ti';
 
 const Analytics = ({ user }) => (
   <React.Fragment>
     <Grid>
-      <Row className="content">
+      <Row className="content user-header">
         <Col xs={12} sm={12} md={6} className="user-bio">
           <img src={user.avatar_url} alt={`Github user ${user.login} pic`} className="user-pic" />
           <div className="user-details">
             <p className="user-name">{user.login}</p>
             <p className="bio">{user.bio}</p>
+          </div>
+        </Col>
+        <Col xs={12} sm={12} md={6} className="user-info">
+          <div className="info">
+            <p className="row value-icon-row">
+              <span className="value">{user.followers}</span>
+              <TiGroup className="info-icon" />
+            </p>
+            <p className="name-row">Followers</p>
           </div>
         </Col>
       </Row>
