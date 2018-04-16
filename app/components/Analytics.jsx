9c982bc08f178b0334b1495856fc75930e7faa97
Naman Kumar <naman@outlook.in>
2018-04-16 12:42:44 +0530

    added cards

--- app/components/Analytics.jsx
+++ app/components/Analytics.jsx
@@ -4,25 +4,50 @@ import { Grid, Row, Col } from 'react-bootstrap';
 const Analytics = ({ user }) => (
   <React.Fragment>
     <Grid>
-      <Row className="content user-header">
-        <Col xs={12} sm={12} md={6} className="user-bio">
-          <div className="user-pic-box">
-            <div className="user-pic-bg">
-              <img src={user.avatar_url} alt={`Github user ${user.login} pic`} className="user-pic" />
+      <section>
+        <Row xs={12} sm={12} className="content">
+          <Col xs={12} sm={12} md={6} className="user-bio">
+            <div className="user-pic-box">
+              <div className="user-pic-bg">
+                <img src={user.avatar_url} alt={`Github user ${user.login} pic`} className="user-pic" />
+              </div>
             </div>
-          </div>
-          <div className="user-details">
-            <h2 className="user-name">{user.login}</h2>
-            <p className="bio">{user.bio}</p>
-          </div>
-        </Col>
-        <Col xs={12} sm={12} md={6} className="user-info">
-          <div className="info">
-            <h2 className="value">{user.followers}</h2>
-            <p className="name">Followers</p>
-          </div>
-        </Col>
-      </Row>
+            <div className="user-details">
+              <a href={user.html_url} target="_blank">
+                <h2 className="user-name">{user.login}</h2>
+              </a>
+              <p className="bio">{user.bio}</p>
+            </div>
+          </Col>
+        </Row>
+      </section>
+      <section>
+        <h3 className="section-head under">I own {user.own_repos} repositories</h3>
+        <Row xs={12} sm={12} className="content">
+          <Col xs={12} sm={12} md={2} className="card-wrap">
+            <div className="card">
+              <Row>
+                <Col className="value">
+                  <h3>{user.commits}</h3>
+                  <p className="name">Commits</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="value">
+                  <h3>{user.stars}</h3>
+                  <p className="name">Stars</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="value">
+                  <h3>{user.forks}</h3>
+                  <p className="name">Forks</p>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </section>
     </Grid>
   </React.Fragment>
 );
