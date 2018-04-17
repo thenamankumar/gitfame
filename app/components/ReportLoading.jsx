5ccaff3aac16412abcb7fef108c0313d81e5295f
Naman Kumar <naman@outlook.in>
2018-04-18 00:25:02 +0530

    Added Loading

--- app/components/ReportLoading.jsx
+++ app/components/ReportLoading.jsx
@@ -1,5 +1,261 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import Animate from './Animate';
 
-const ReportLoading = () => <div />;
+const ReportLoading = () => (
+  <React.Fragment>
+    <Animate name="fadeInOut" timeout={1000000}>
+      <Grid className="animated slow infinite loading-wrap">
+        <section>
+          <Row xs={12} sm={12} className="content">
+            <Col xs={12} sm={12} md={6} className="user-bio">
+              <div className="user-pic-box">
+                <div className="user-pic-bg">
+                  <div className="shape user-pic" />
+                </div>
+              </div>
+              <div className="user-details">
+                <div className="shape s-h2" />
+                <div className="shape s-p" />
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={6}>
+              <Row className="right">
+                <Col xs={2} sm={2} className="text-center pad-5">
+                  <div className="shape s-h2" />
+                  <div className="shape s-p" />
+                </Col>
+                <Col xs={2} sm={2} className="text-center pad-5">
+                  <div className="shape s-h2" />
+                  <div className="shape s-p" />
+                </Col>
+                <Col xs={2} sm={2} className="text-center pad-5">
+                  <div className="shape s-h2" />
+                  <div className="shape s-p" />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </section>
+        <section className="slim">
+          <Row className="content">
+            <Col xs={12} sm={12} md={2} className="card-wrap">
+              <div className="card">
+                <Row className="center slim">
+                  <Col xs={12} sm={12} className="pad-15">
+                    <div className="shape s-h2" />
+                    <div className="shape s-p" />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={2} className="card-wrap">
+              <div className="card">
+                <Row className="center slim">
+                  <Col xs={12} sm={12} className="pad-15">
+                    <div className="shape s-h2" />
+                    <div className="shape s-p" />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={2} className="card-wrap">
+              <div className="card">
+                <Row className="center slim">
+                  <Col xs={12} sm={12} className="pad-15">
+                    <div className="shape s-h2" />
+                    <div className="shape s-p" />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={2} className="card-wrap">
+              <div className="card">
+                <Row className="center slim">
+                  <Col xs={12} sm={12} className="pad-15">
+                    <div className="shape s-h2" />
+                    <div className="shape s-p" />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={2} className="card-wrap">
+              <div className="card">
+                <Row className="center slim">
+                  <Col xs={12} sm={12} className="pad-15">
+                    <div className="shape s-h2" />
+                    <div className="shape s-p" />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={2} className="card-wrap">
+              <div className="card">
+                <Row className="center slim">
+                  <Col xs={12} sm={12} className="pad-15">
+                    <div className="shape s-h2" />
+                    <div className="shape s-p" />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </section>
+        <section>
+          <div className="section-head">
+            <div className="shape s-h3 w-300" />
+          </div>
+          <Row className="content">
+            <Col xs={12} sm={12} md={6} className="card-wrap">
+              <div className="card tag total">
+                <Row>
+                  <div className="shape s-h4 w-200" />
+                </Row>
+                <Row>
+                  <div className="shape s-p" />
+                </Row>
+                <Row className="center">
+                  <Col xs={7} sm={7}>
+                    <div className="shape chart" />
+                  </Col>
+                  <Col xs={5} sm={5} className="labels-list-wrap">
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                  </Col>{' '}
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={6} className="card-wrap">
+              <div className="card tag owned">
+                <Row>
+                  <div className="shape s-h4 w-200" />
+                </Row>
+                <Row>
+                  <div className="shape s-p" />
+                </Row>
+                <Row className="center">
+                  <Col xs={7} sm={7}>
+                    <div className="shape chart" />
+                  </Col>
+                  <Col xs={5} sm={5} className="labels-list-wrap">
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </section>
+        <section>
+          <div className="section-head">
+            <div className="shape s-h3 w-300" />
+          </div>
+          <Row className="content">
+            <Col xs={12} sm={12} md={6} className="card-wrap">
+              <div className="card tag total">
+                <Row>
+                  <div className="shape s-h4 w-200" />
+                </Row>
+                <Row className="center">
+                  <Col xs={7} sm={7}>
+                    <div className="shape chart" />
+                  </Col>
+                  <Col xs={5} sm={5} className="labels-list-wrap">
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                  </Col>{' '}
+                </Row>
+                <Row>
+                  <div className="shape s-p" />
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={6} className="card-wrap">
+              <div className="card tag total">
+                <Row>
+                  <div className="shape s-h4 w-200" />
+                </Row>
+                <Row className="center">
+                  <Col xs={7} sm={7}>
+                    <div className="shape chart" />
+                  </Col>
+                  <Col xs={5} sm={5} className="labels-list-wrap">
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                  </Col>{' '}
+                </Row>
+                <Row>
+                  <div className="shape s-p" />
+                </Row>
+              </div>
+            </Col>
+          </Row>
+          <Row className="content">
+            <Col xs={12} sm={12} md={6} className="card-wrap">
+              <div className="card tag owned">
+                <Row>
+                  <div className="shape s-h4 w-200" />
+                </Row>
+                <Row className="center">
+                  <Col xs={7} sm={7}>
+                    <div className="shape chart" />
+                  </Col>
+                  <Col xs={5} sm={5} className="labels-list-wrap">
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                  </Col>{' '}
+                </Row>
+                <Row>
+                  <div className="shape s-p" />
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={6} className="card-wrap">
+              <div className="card tag owned">
+                <Row>
+                  <div className="shape s-h4 w-200" />
+                </Row>
+                <Row className="center">
+                  <Col xs={7} sm={7}>
+                    <div className="shape chart" />
+                  </Col>
+                  <Col xs={5} sm={5} className="labels-list-wrap">
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                    <div className="shape s-p" />
+                  </Col>
+                </Row>
+                <Row>
+                  <div className="shape s-p" />
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </section>
+      </Grid>
+    </Animate>
+    <div className="overlay text-center">
+      <div className="loading-msg">
+        <div className="lds-ring">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+    </div>
+  </React.Fragment>
+);
 
 export default ReportLoading;
