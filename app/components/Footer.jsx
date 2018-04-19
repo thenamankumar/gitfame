640418fb28dfd1dc2a4a02f5437149408800d7e0
Naman Kumar <naman@outlook.in>
2018-04-19 16:03:09 +0530

    added credits

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import { Grid } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
+import { TiHeart } from 'react-icons/lib/ti';
 
-const Footer = () => <Grid className="footer" />;
+const Footer = () => (
+  <Grid className="footer">
+    <Row className="center">
+      <p>
+        Made with <TiHeart className="heart" /> by{' '}
+        <a href="https://github.com/hereisnaman" target="_blank" rel="noreferrer noopener">
+          Naman Kumar
+        </a>
+      </p>
+    </Row>
+  </Grid>
+);
 
 export default Footer;
